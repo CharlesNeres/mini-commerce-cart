f2d82cb56d831cfb58fe278d9865c580978872a6
Charles Oliveira <charlesbritoneresjr@gmail.com>
2026-05-10 21:32:59 +0800

    feat: add products to cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
 import { products } from "./data/products";
+import { useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(product) {
+    setCart([...cart, product]);
+  }
+
   return (
     <main>
       <h1>Mini Commerce Cart</h1>
@@ -14,6 +21,7 @@ function App() {
             <h2>{product.name}</h2>
             <p>{product.category}</p>
             <strong>${product.price.toFixed(2)}</strong>
+            <button onClick={() => addToCart(product)}>Add to cart</button>
           </article>
         ))}
       </section>
