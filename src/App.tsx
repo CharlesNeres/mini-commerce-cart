85c4152b79b825764301e3ea930b12724b88cc78
Charles Oliveira <charlesbritoneresjr@gmail.com>
2026-05-11 19:26:20 +0800

    feat: render shopiping cart items

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,26 @@ function App() {
   const [cart, setCart] = useState([]);
 
   function addToCart(product) {
-    setCart([...cart, product]);
+    const productExists = cart.find((item) => item.id === product.id);
+
+    if (productExists) {
+      const updatedCart = cart.map((item) => {
+        if (item.id === product.id) {
+          return {
+            ...item,
+            quantity: item.quantity + 1,
+          };
+        }
+
+        return item;
+      });
+
+      setCart(updatedCart);
+
+      return;
+    }
+
+    setCart([...cart, { ...product, quantity: 1 }]);
   }
 
   return (
@@ -22,6 +41,20 @@ function App() {
             <p>{product.category}</p>
             <strong>${product.price.toFixed(2)}</strong>
             <button onClick={() => addToCart(product)}>Add to cart</button>
+          </article>
+        ))}
+      </section>
+
+      <section className="cart">
+        <h2>Cart</h2>
+        {cart.map((item) => (
+          <article key={item.id} className="cart-item">
+            <div>
+              <h3>{item.name}</h3>
+              <p>Quantity: {item.quantity}</p>
+            </div>
+
+            <strong>${(item.price * item.quantity).toFixed(2)}</strong>
           </article>
         ))}
       </section>
